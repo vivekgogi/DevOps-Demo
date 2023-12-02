d463b9448f6beb5d20ab34c6904e26c0f58e6da3
vivekgogi <114925625+vivekgogi@users.noreply.github.com>
2023-12-02 20:43:53 +0530

    Create fast_api_app.py

---
+++ fast_api_app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/hello")
+def hello_world(text: str):
+    return {"message": "Hello World!"}
+
+# To run fast api command: uvicorn fast_api_app:app --reload
