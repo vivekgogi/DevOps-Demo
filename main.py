e2062c8873b010afed63a4ce078f11514797cbb4
Laxmikurapati <dlaxmikurapati@gmail.com>
2023-12-11 10:55:22 +0530

    First commit

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = FastAPI()
 
 @app.post("/hello")
 def hello_world(text: str):
-    return {"message": "Hello World!"}
+    return {"message": "Hello Universe!"}
 
 # To run fast api command: uvicorn fast_api_app:app --reload
